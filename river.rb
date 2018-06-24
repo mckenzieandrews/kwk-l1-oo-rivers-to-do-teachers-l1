3c92ae426b32485bd9aa6de6118deb0db1943996
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-06-24 02:14:16 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,15 @@
 # river.rb
+class River
+  attr_accessor :name, :length, :countries, :discharge
+  def initialize(name)
+    @name = name
+  end
+
+  def flood
+    @discharge = @discharge * 1.30
+  end
+
+  def dry_up
+    @discharge = @discharge * 0.5
+  end
+end
